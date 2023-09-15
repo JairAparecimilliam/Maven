2582df4212277f4b8acffa3d94a149fbfb7029ce
Jair <jrabc1991@gmail.com>
2023-09-15 13:29:55 -0300

    Update Programa

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -1,29 +1,25 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import dominio.Pessoa;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(null, "Carlos da silva", "Carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Moizes Tiago", "Moizes@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Antônio Fagundes", "Antonio@gmail.com");
-
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		
+		Pessoa p = em.find(Pessoa.class, 2);
+		/*
+		 * para apagar uma pessoa do banco de dados em.remove(p);
+		 */
+		System.out.println(p);
 		System.out.println("OK");
-	}
+		em.close();
+		emf.close();
 
+	}
 }
