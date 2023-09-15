b24f5f81b78f65b6858e83370600fb8a07ad0c32
Jair <jrabc1991@gmail.com>
2023-09-15 00:33:17 -0300

    Populated Data Banking

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -11,16 +11,19 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Carlos da silva", "Carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Moizes Tiago", "Moizes@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Antônio Fagundes", "Antonio@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Carlos da silva", "Carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Moizes Tiago", "Moizes@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Antônio Fagundes", "Antonio@gmail.com");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit-jpa");
 		EntityManager em = emf.createEntityManager();
-
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		
+		System.out.println("OK");
 	}
 
 }
