9cdaac0f34b2f5ff1dad3967f0c86853e06bf48b
Jair <jrabc1991@gmail.com>
2023-09-15 00:21:05 -0300

    Intaciate EntityManager

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -3,6 +3,8 @@ package application;
 import javax.persistence.Persistence;
 
 import dominio.Pessoa;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class Programa {
@@ -14,6 +16,8 @@ public class Programa {
 		Pessoa p3 = new Pessoa(3, "Antônio Fagundes", "Antonio@gmail.com");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit-jpa");
+		EntityManager em = emf.createEntityManager();
+
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
